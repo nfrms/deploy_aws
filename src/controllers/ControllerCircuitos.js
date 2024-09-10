fe9973b870f849d247bd518b528202e7a06022d3
nfrms <nfrms@iscte-iul.pt>
2024-09-10 19:45:46 +0100

    decimo terceiro commit

--- src/controllers/ControllerCircuitos.js
+++ src/controllers/ControllerCircuitos.js
@@ -1,7 +1,7 @@
 const sequelize = require('sequelize');
 const database = require('../db');
 // const BASE_URL = 'http://192.168.1.70:4200';
- const BASE_URL = 'http://52.56.53.61:4200';
+ const BASE_URL = 'http://3.8.176.170:4200';
 
 module.exports =
 {
